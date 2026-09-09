96675ddaf718958e247a0d2a747b19c415d8f8f2
Biao <40375808+Bwy999@users.noreply.github.com>
2026-09-09 16:24:20 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "btc-intelligence-v25-1-4-ui-audit-20260908";
+const CACHE_NAME = "btc-intelligence-v25-0-1-compact-cost-20260908";
 const CORE = [
   "./",
   "./index.html",
